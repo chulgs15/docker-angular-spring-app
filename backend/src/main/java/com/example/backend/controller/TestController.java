1dda2fcb5ba7f9ef48cea7a34e851a1e6aba46f5
shin <chulgs15@naver.com>
2021-05-20 22:13:34 +0900

    commit

--- backend/src/main/java/com/example/backend/controller/TestController.java
+++ backend/src/main/java/com/example/backend/controller/TestController.java
@@ -1,13 +1,19 @@
 package com.example.backend.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
-    @GetMapping(value = {"/hello"})
+    @GetMapping(value = {"/api/hello"})
     public String helloWorld() {
         return "hello world";
+    }
+
+    @GetMapping(value = {"/api/hello/{name}"})
+    public String helloWorld(@PathVariable(value = "name") String name) {
+        return "hello world " + name;
     }
 }
--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontend';
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  public clickButton(): void {
+    this.http.get<String>("localhost:8080/api/hello")
+      .subscribe(
+        value => {
+          alert(value);
+        }
+      );
+  }
 }
